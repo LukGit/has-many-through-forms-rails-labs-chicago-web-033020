244dc6f8fe1fe7da20d5958868bec81bb0ce6483
Shau Yin Luk <ivan.luk@icloud.com>
2020-04-28 21:39:17 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
+  # accepts_nested_attributes_for :comments, reject_if: proc { |attributes| attributes['content'].blank? }
+
+
+  def users_attributes=(user_attributes)
+    user_attributes.values.each do |user_attribute|
+      if !user_attribute[:username].empty?
+        user = User.find_or_create_by(user_attribute)
+        self.users << user
+      end
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+cat1 = Category.create([{name: 'Sad'}, {name: 'Happy'}, {name: 'Netural'}])
